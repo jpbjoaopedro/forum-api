2bece0b65609f73c3aab562cecfc9bd57af6c87c
Joao Pedro Bandeira <jbandeira@detro.rj.gov.br>
2025-09-16 14:43:15 -0300

    feat: add JWT authentication to AuthModule and update signIn method in AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '24h' }
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule { }
