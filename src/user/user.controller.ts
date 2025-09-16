9c2ca4e587235fff91b31b63d899b84182e843be
Joao Pedro Bandeira <jbandeira@detro.rj.gov.br>
2025-09-16 11:10:39 -0300

    refactor: improve code formatting and ensure consistent indentation in user module and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,35 +1,44 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
-    @Get(':id')
-    async getUser(@Param('id') id: string): Promise<UserModel | null> {
-        return this.userService.user({ id: Number(id) });
-    }
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel | null> {
+    return this.userService.user({ id: Number(id) });
+  }
 
-    @Post()
-    async signupUser(
-        @Body() userData: Prisma.UserCreateInput,
-    ): Promise<UserModel> {
-        return this.userService.createUser(userData);
-    }
+  @Post()
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
 
-    @Put(':id')
-    async updateUser(
-        @Body() userData: Prisma.UserUpdateInput,
-        @Param('id') id: string
-    ): Promise<UserModel> {
-        return this.userService.updateUser({ where: { id: Number(id) }, data: userData })
-    }
+  @Put(':id')
+  async updateUser(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
 
-    @Delete(':id')
-    async DeleteUser(
-        @Param('id') id: string
-    ): Promise<UserModel> {
-        return this.userService.deleteUser({ id: Number(id) })
-    }
+  @Delete(':id')
+  async DeleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,47 +3,48 @@ import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 import * as bcrypt from 'bcrypt';
 
-
 @Injectable()
 export class UserService {
-    @Inject()
-    private readonly prisma: PrismaService;
+  @Inject()
+  private readonly prisma: PrismaService;
 
-    async user(
-        userWhereUniqueInput: Prisma.UserWhereUniqueInput,
-    ): Promise<User | null> {
-        return this.prisma.user.findUnique({
-            where: userWhereUniqueInput,
-        });
-    }
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
 
-    async createUser(data: Prisma.UserCreateInput) {
-        const saltOrRounds = 10;
-        const password = data.password;
-        const hashPassword = await bcrypt.hash(password, saltOrRounds);
+  async createUser(data: Prisma.UserCreateInput) {
+    const saltOrRounds = 10;
+    const password = data.password;
+    const hashPassword = await bcrypt.hash(password, saltOrRounds);
 
-        return this.prisma.user.create({ data: { ...data, password: hashPassword } })
-    }
+    return this.prisma.user.create({
+      data: { ...data, password: hashPassword },
+    });
+  }
 
-    async updateUser(params: {
-        where: Prisma.UserWhereUniqueInput;
-        data: Prisma.UserUpdateInput;
-    }): Promise<User> {
-        const { where, data } = params;
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
 
-        const saltOrRounds = 10;
-        const password = data.password as string;
-        const hashPassword = await bcrypt.hash(password, saltOrRounds);
+    const saltOrRounds = 10;
+    const password = data.password as string;
+    const hashPassword = await bcrypt.hash(password, saltOrRounds);
 
-        return this.prisma.user.update({
-            data: { ...data, password: hashPassword },
-            where,
-        });
-    }
+    return this.prisma.user.update({
+      data: { ...data, password: hashPassword },
+      where,
+    });
+  }
 
-    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-        return this.prisma.user.delete({
-            where,
-        });
-    }
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where,
+    });
+  }
 }
