bf0eca103d133b4356225d5a4fd39050156faa3f
Joao Pedro Bandeira <jbandeira@detro.rj.gov.br>
2025-09-17 13:15:41 -0300

    feat: add DatabaseModule import to Answers and Questions modules

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [QuestionsController],
+  imports: [DatabaseModule],
   providers: [QuestionsService],
 })
 export class QuestionsModule {}
