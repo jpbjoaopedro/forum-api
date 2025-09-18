f76389e30406c94c576e1bd9f2cc68269044dc54
João Pedro Bandeira <jpbandeiradossantos@gmail.com>
2025-09-18 14:54:20 -0300

    feat: add validation decorators to CreateAnswerDto and CreateQuestionDto for improved input validation

--- src/answers/dto/create-answer.dto.ts
+++ src/answers/dto/create-answer.dto.ts
@@ -1,3 +1,8 @@
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateAnswerDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
   body: string
 }
--- src/questions/dto/create-question.dto.ts
+++ src/questions/dto/create-question.dto.ts
@@ -1,4 +1,13 @@
+import { IsNotEmpty, IsString, MinLength } from "class-validator"
+
 export class CreateQuestionDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
   title: string
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(20)
   body: string
 }
